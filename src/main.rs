f88c115c0968b0887940617dab0599db23aa2bcd
Rudolph Lorenz <rudlorenz@gmail.com>
2020-12-16 19:52:24 +0700

    Move rendering to separate function

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,11 @@
 mod structs;
 
 use crate::structs::hitable::*;
-use crate::structs::ray::Ray;
 use crate::structs::vec3::Vec3;
 use crate::structs::viewport::Viewport;
 
-use rayon::prelude::*;
-
 extern crate image;
 use image::{ImageBuffer, Rgb};
-
-use rand::prelude::*;
-
-fn ray_col<R: Rng>(r: &Ray, world: &HitList, rng: &mut R, depth: u32) -> Vec3 {
-    if depth != 0 {
-        match world.hit(r, 0.001, f64::MAX) {
-            Some(hit_rec) => {
-                if let Some(scatter_vec) = hit_rec.material_.scatter(&r, &hit_rec, rng) {
-                    hit_rec.material_.attenuation() * ray_col(&scatter_vec, &world, rng, depth - 1)
-                } else {
-                    Vec3::new(0., 0., 0.)
-                }
-            }
-            None => {
-                let t = 0.5 * (Vec3::unit_vector(&r.direction()).y_ + 1.);
-                (1. - t) * Vec3::new(1., 1., 1.) + t * Vec3::new(0.5, 0.7, 1.0)
-            }
-        }
-    } else {
-        Vec3::new(0., 0., 0.)
-    }
-}
 
 fn main() {
     let aspect_ratio = 16. / 9.;
@@ -43,7 +18,6 @@ fn main() {
     let lookat = Vec3::new(0., 0., 0.);
     let vup = Vec3::new(0., 1., 0.);
     let dist_to_focus = (lookfrom - lookat).length();
-    //let dist_to_focus =  10.;
     let aperture = 0.2;
     let vertical_fov = 40.;
 
@@ -57,36 +31,13 @@ fn main() {
         dist_to_focus,
     );
 
-    let hit_listy = HitList::random_scene();
-
-    let raw_pixels: Vec<u8> = (0..img_height)
-        .into_par_iter()
-        .rev()
-        .map_init(rand::thread_rng, |rng, j| {
-            let mut rslt = Vec::with_capacity(3 * img_width as usize);
-            for i in 0..img_width {
-                let mut col = Vec3::new(0., 0., 0.);
-                for _ns in 0..samples {
-                    let u = (i as f64 + rng.gen::<f64>()) / img_width as f64;
-                    let v = (j as f64 + rng.gen::<f64>()) / img_height as f64;
-                    let r = viewport.send_ray(u, v, rng);
-                    col = col + ray_col(&r, &hit_listy, rng, ray_depth);
-                }
-
-                col = col / samples as f64;
-
-                let ir = (256. * num::clamp(col.x_.sqrt(), 0., 0.999)) as u8;
-                let ig = (256. * num::clamp(col.y_.sqrt(), 0., 0.999)) as u8;
-                let ib = (256. * num::clamp(col.z_.sqrt(), 0., 0.999)) as u8;
-
-                rslt.push(ir);
-                rslt.push(ig);
-                rslt.push(ib);
-            }
-            rslt
-        })
-        .flatten()
-        .collect();
+    let raw_pixels = viewport.render(
+        img_width,
+        img_height,
+        samples,
+        ray_depth,
+        &HitList::random_scene(),
+    );
 
     let img_buf: ImageBuffer<Rgb<u8>, Vec<u8>> =
         image::ImageBuffer::from_vec(img_width, img_height, raw_pixels).unwrap();
--- src/structs/viewport.rs
+++ src/structs/viewport.rs
@@ -1,6 +1,10 @@
+use crate::structs::hitable::{HitList, Hitable};
 use crate::structs::ray::Ray;
 use crate::structs::vec3::{Point3, Vec3};
+
 use rand::Rng;
+
+use rayon::prelude::*;
 
 pub struct Viewport {
     lens_radius_: f64,
@@ -55,5 +59,64 @@ impl Viewport {
                 - self.origin_
                 - offset),
         )
+    }
+
+    fn ray_col<R: Rng>(r: &Ray, scene: &HitList, rng: &mut R, depth: u32) -> Vec3 {
+        if depth != 0 {
+            match scene.hit(r, 0.001, f64::MAX) {
+                Some(hit_rec) => {
+                    if let Some(scatter_vec) = hit_rec.material_.scatter(&r, &hit_rec, rng) {
+                        hit_rec.material_.attenuation()
+                            * Viewport::ray_col(&scatter_vec, &scene, rng, depth - 1)
+                    } else {
+                        Vec3::new(0., 0., 0.)
+                    }
+                }
+                None => {
+                    let t = 0.5 * (Vec3::unit_vector(&r.direction()).y_ + 1.);
+                    (1. - t) * Vec3::new(1., 1., 1.) + t * Vec3::new(0.5, 0.7, 1.0)
+                }
+            }
+        } else {
+            Vec3::new(0., 0., 0.)
+        }
+    }
+
+    pub fn render(
+        &self,
+        img_width: u32,
+        img_height: u32,
+        samples: u32,
+        ray_depth: u32,
+        scene: &HitList,
+    ) -> Vec<u8> {
+        (0..img_height)
+            .into_par_iter()
+            .rev()
+            .map_init(rand::thread_rng, |rng, j| {
+                let mut result = Vec::with_capacity(3 * img_width as usize);
+                for i in 0..img_width {
+                    let mut col = Vec3::new(0., 0., 0.);
+                    for _ns in 0..samples {
+                        let u = (i as f64 + rng.gen::<f64>()) / img_width as f64;
+                        let v = (j as f64 + rng.gen::<f64>()) / img_height as f64;
+                        let r = self.send_ray(u, v, rng);
+                        col = col + Viewport::ray_col(&r, &scene, rng, ray_depth);
+                    }
+
+                    col = col / samples as f64;
+
+                    let ir = (256. * num::clamp(col.x_.sqrt(), 0., 0.999)) as u8;
+                    let ig = (256. * num::clamp(col.y_.sqrt(), 0., 0.999)) as u8;
+                    let ib = (256. * num::clamp(col.z_.sqrt(), 0., 0.999)) as u8;
+
+                    result.push(ir);
+                    result.push(ig);
+                    result.push(ib);
+                }
+                result
+            })
+            .flatten()
+            .collect()
     }
 }
